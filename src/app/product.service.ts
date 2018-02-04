9b29b3fa5546548fbbcd75d191050a7809cb52c0
Narayana Chitti <nchitti@firstcdn.com>
2018-02-04 18:09:07 -0500

    Part 6 - Steps 1 to 17

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,12 +4,13 @@ import  'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
-
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl : string = '../assets/album.json';
+  private _productsUrl : string = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -18,5 +19,11 @@ export class ProductService {
     return this._http
                .get(this._albumUrl)
                .map(response => <Album>response.json());
+  }
+
+  getProducts() : Observable<Product[]> {
+    return this._http
+               .get(this._productsUrl)
+               .map(response => <Product[]>response.json());
   }
 }
